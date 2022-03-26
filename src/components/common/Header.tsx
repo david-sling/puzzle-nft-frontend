3d0ddb23ae102b9ea1d72667a43444f194256015
David S D <sddavid.johan@gmail.com>
2022-03-26 11:19:14 +0530

    UI : header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,9 +5,22 @@ import Section from "./Section";
 
 export default function Header() {
   return (
-    <Section className={classes.container}>
-      <Logo />
-      HEADER
+    <Section
+      className={classes.container}
+      innerClassName={classes.innerContainer}
+    >
+      <div className={classes.header}>
+        <div className={classes.logo}>
+          <Logo />
+          <h1>Ennefti</h1>
+        </div>
+        <nav className={classes.nav}>
+          <p>ABOUT US</p>
+          <p>MY PUZZLE</p>
+          <p>MARKETPLACE</p>
+          <button>MINT</button>
+        </nav>
+      </div>
     </Section>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,5 +1,4 @@
 import { ExternalProvider } from "@ethersproject/providers";
-import { MetaMaskInpageProvider } from "@metamask/providers";
 /// <reference types="react-scripts" />
 
 interface Provider extends ExternalProvider {
