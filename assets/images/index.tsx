2b9ab37dc134a4973821c069ff2e670f2d32629c
David S D <sddavid.johan@gmail.com>
2022-03-27 20:34:48 +0530

    feat : ctas

--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -3,6 +3,8 @@ import { FC, SVGProps } from "react";
 // Import SVG files
 import logo from "./logo.svg";
 import footerBg from "./footerBg.svg";
+import roundArrow from "./roundArrow.svg";
+import polygon from "./polygon.svg";
 
 const makeSvgComponent = (Svg: any, initialProps?: SVGProps<SVGSVGElement>) =>
   ((props) => (
@@ -20,3 +22,5 @@ const makeSvgComponent = (Svg: any, initialProps?: SVGProps<SVGSVGElement>) =>
 // Export SVG files
 export const Logo = makeSvgComponent(logo, { height: 23, width: 28 });
 export const FooterBg = makeSvgComponent(footerBg);
+export const RoundArrow = makeSvgComponent(roundArrow);
+export const Polygon = makeSvgComponent(polygon);
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,3 +1,4 @@
+import { Polygon, RoundArrow } from "assets/images";
 import Section from "components/common/Section";
 import { Puzzle } from "config/ethereum";
 import { FC, useEffect, useState } from "react";
@@ -15,13 +16,40 @@ const Hero: FC = () => {
       className={classes.container}
       innerClassName={classes.innerContainer}
     >
-      <div className={classes.left}>
-        <h1>
-          World&apos;s <span>First and Only</span> NFT puzzle
-        </h1>
+      <div className={classes.top}>
+        <div className={classes.text}>
+          <h1>
+            World&apos;s <span>First and Only</span> NFT puzzle
+          </h1>
+        </div>
+        <div className={classes.canvas}>
+          {typeof window !== "undefined" && (
+            <iframe src="/matter.html"></iframe>
+          )}
+        </div>
       </div>
-      <div className={classes.right}>
-        <iframe src="/matter.html"></iframe>
+      <div className={classes.ctas}>
+        <button>
+          <div className={classes.ps}>
+            <p className={classes.p1}>Mint a random piece for</p>
+            <div className={classes.polygon}>
+              <Polygon />
+              <p className={classes.p2}>9.0</p>
+            </div>
+          </div>
+          <div className={classes.arrow}>
+            <RoundArrow />
+          </div>
+        </button>
+        <button>
+          <div className={classes.ps}>
+            <p className={classes.p1}>Go to Ennefti&apos;s</p>
+            <p className={classes.p2}>Marketplace</p>
+          </div>
+          <div className={classes.arrow}>
+            <RoundArrow />
+          </div>
+        </button>
       </div>
     </Section>
   );
